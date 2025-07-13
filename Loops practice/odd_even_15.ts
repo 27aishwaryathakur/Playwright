ec1251fa2cd08fbfdfe85ea4e0f95a1fa8896672
27aishwaryathakur <71473575+27aishwaryathakur@users.noreply.github.com>
2025-07-13 16:55:44 +0530

    removing to add from OG

--- Loops practice/odd_even_15.ts
+++
@@ -1,8 +0,0 @@
-//To print 1 to 15 and segregate if they are even or odd
-
-for(let q : number=0;q<=15;q++)
-{if((q % 2===0) || q===0)
-{console.log((q)+" is even")}
-else
-{console.log((q)+" is odd")}
-}
--- print.ts
+++
@@ -1,2 +0,0 @@
-//tsx-- typescript compiler
-console.log(10,"AISH",true);
